9678547b10fa4fcb6f94962c942aacfff08009ed
Alin Sanchez <edalin.sb.2908@gmail.com>
2020-09-24 15:47:43 -0500

    Login conected

--- app/src/main/java/com/ram/examen/ui/activity/login/LoginActivity.kt
+++ app/src/main/java/com/ram/examen/ui/activity/login/LoginActivity.kt
@@ -1,4 +1,16 @@
 package com.ram.examen.ui.activity.login
 
-class LoginActivity {
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.ram.examen.R
+
+class LoginActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_login)
+    }
+
+
+
 }
--- app/src/main/java/com/ram/examen/ui/activity/login/fragment/LoginFragment.kt
+++ app/src/main/java/com/ram/examen/ui/activity/login/fragment/LoginFragment.kt
@@ -1,4 +1,26 @@
 package com.ram.examen.ui.activity.login.fragment
 
-class LoginFragment {
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.ram.examen.databinding.FragmentLoginBinding
+
+class LoginFragment : Fragment() {
+
+    lateinit var binding: FragmentLoginBinding
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        binding = FragmentLoginBinding.inflate(inflater)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+    }
 }
